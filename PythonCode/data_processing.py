1569671990b640054121686eba233aa35a30b5b3
petitlang <L2164753957@gmail.com>
2024-04-05 11:57:33 +0200

    commentaire francais

--- PythonCode/data_processing.py
+++ PythonCode/data_processing.py
@@ -1,11 +1,14 @@
-# 处理从Arduino设备接收的数据，可能包括解析、存储和准备数据以供仪表板使用。
+'''
+Traitement des données reçues des dispositifs Arduino, ce qui peut inclure l'analyse,
+le stockage et la préparation des données en vue de leur utilisation dans les tableaux de bord.
+'''
 import sqlite3
 import paho.mqtt.client as mqtt
 
-# 数据库文件路径
+# path database
 db_path = 'animal_tracking.db'
 
-# 检查并创建表
+# verifier et creer la tableau
 def setup_database():
     conn = sqlite3.connect(db_path)
     c = conn.cursor()
@@ -23,37 +26,37 @@ def setup_database():
 def update_database(name, x, y, temperature):
     conn = sqlite3.connect(db_path)
     c = conn.cursor()
-    # 使用REPLACE来更新或插入新记录
+    # update data par REPLACE
     c.execute('REPLACE INTO animal_data (name, x, y, temperature) VALUES (?, ?, ?, ?)',
               (name, x, y, temperature))
     conn.commit()
     conn.close()
 
-# MQTT消息回调
+# MQTT_ON_MSG
 def on_message(client, userdata, msg):
     valeur = msg.payload.decode("utf-8")
     print(msg.topic + " " + valeur)
     
     try:
         elements = valeur.split(":")
-        name = elements[0]  # 动物名
-        x = int(elements[1])  # X坐标
-        y = int(elements[2])  # Y坐标
+        name = elements[0]  # nom animal
+        x = int(elements[1])  # X coordoneé
+        y = int(elements[2])  # Y coordoneé
         temperature = None
         if len(elements) > 3 and elements[3].startswith('T='):
             temperature = float(elements[3][2:])
         
-        # 更新数据库
+        # update database
         update_database(name, x, y, temperature)
     except Exception as e:
         print(f"Error processing message - {e}")
 
 def main():
-    # 检查并设置数据库
+    #  Vérification et configuration de la base de données
     setup_database()
     
     client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, "Panda")
-    client.connect("srv-lora.isep.fr")  # 以实际使用的broker地址和端口替换
+    client.connect("srv-lora.isep.fr")
     client.subscribe("Panda")
     client.on_message = on_message
     client.loop_forever()
